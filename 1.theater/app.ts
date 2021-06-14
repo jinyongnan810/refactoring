f823ac0bda8dfccffbcfc4fe655c374f71fcef1f
Yuunan Kin <yuunan.kin@gmail.com>
2021-06-15 06:29:16 +0900

    rename vars in extracted function

--- 1.theater/app.ts
+++ 1.theater/app.ts
@@ -1,26 +1,26 @@
 import { Invoice, Perfomance, Play, SinglePlay } from "./typings";
 
 // 金額計算ロジック
-const amountFor = (perf: Perfomance, play: SinglePlay) => {
-  let thisAmount = 0;
+const amountFor = (aPerformance: Perfomance, play: SinglePlay) => {
+  let result = 0;
   switch (play.type) {
     case "tragedy":
-      thisAmount = 40000;
-      if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
+      result = 40000;
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
       }
       break;
     case "comedy":
-      thisAmount = 30000;
-      if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20);
+      result = 30000;
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
       }
-      thisAmount += 300 * perf.audience;
+      result += 300 * aPerformance.audience;
       break;
     default:
       throw new Error(`Unknown type:${play.type}`);
   }
-  return thisAmount;
+  return result;
 };
 
 export const statement = (invoice: Invoice, plays: Play) => {
